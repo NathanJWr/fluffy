c268c8735977dab57b358a94e99ef77586a333f6
nicfv <omg.my.junkmail@gmail.com>
2020-09-14 12:47:20 -0700

    Print expected identifiers and ints

--- test/lexerTests.c
+++ test/lexerTests.c
@@ -10,20 +10,32 @@ void testLexer(const char *str, int numTypes, ...) {
     char *StringStore = malloc(StringStoreSize);
     token_type type, expectedType;
     int i;
+    char* expectedString;
+    long expectedInt;
     va_list expectedTypes;
     va_start(expectedTypes, numTypes);
 
     LexerInit(&Lexer, &str[0], &str[strlen(str)], StringStore, StringStoreSize);
 
-    printf("%s\n", str);
+    printf("\n%s\n\n", str);
     for(i = 0; i < numTypes; i++) {
         expectedType = va_arg(expectedTypes, token_type);
         type = NextToken(&Lexer);
         printf("  [%d]: %s ? %s\n", i+1, TokenType[expectedType], TokenType[type]);
         assert(type == expectedType);
-        if(expectedType == TOKEN_IDENT) {
-            assert(0 == strcmp(Lexer.String, va_arg(expectedTypes, char*)));
-            printf("   | %s\n", Lexer.String);
+        switch (expectedType) {
+        case TOKEN_IDENT:
+            expectedString = va_arg(expectedTypes, char*);
+            printf("   | %s ? %s\n", expectedString, Lexer.String);
+            assert(0 == strcmp(Lexer.String, expectedString));
+            break;
+        case TOKEN_INT:
+            expectedInt = va_arg(expectedTypes, long);
+            printf("   | %ld ? %ld\n", expectedInt, Lexer.Integer);
+            assert(Lexer.Integer == expectedInt);
+            break;
+        default:
+            break;
         }
     }
     va_end(expectedTypes);
